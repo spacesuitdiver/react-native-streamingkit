82fc235432e89e03e9ad787faf4f60fd3c86ac76
Rajdeep Deb <rajdeepdebcompsci@gmail.com>
2016-09-13 00:02:09 +0530

    keep the music paused if it was due to an interruption

--- android/src/main/java/com/leblaaanc/RNStreamingKitManager/RNStreamingKitManagerModule.java
+++ android/src/main/java/com/leblaaanc/RNStreamingKitManager/RNStreamingKitManagerModule.java
@@ -48,6 +48,7 @@ MediaPlayer.OnErrorListener, MediaPlayer.OnBufferingUpdateListener {
 
   volatile boolean _isPaused;
   volatile boolean _isBuffering;
+  volatile boolean _isCall;
   AudioManager _audioManager;
 ;
 
@@ -136,6 +137,10 @@ MediaPlayer.OnErrorListener, MediaPlayer.OnBufferingUpdateListener {
 
   @ReactMethod
   public void resume() {
+    if(_isCall) {
+      _isCall = false;
+      return;
+    }
     Log.d(NAME, "==> resume");
     if (!isMusicPlaying()) {
         startPlaying();
@@ -270,12 +275,14 @@ MediaPlayer.OnErrorListener, MediaPlayer.OnBufferingUpdateListener {
                     break;
                   case AudioManager.AUDIOFOCUS_GAIN:
                     Log.d(NAME, "==> Audio Session Interruption case AUDIOFOCUS_GAIN.");
-                    resume();
+//                    not calling resume as the media manager will also do that
+//                    resume();
                     notifyAudioInterruption("interruptEnd");
                     break;
                   case AudioManager.AUDIOFOCUS_LOSS:
                     //_audioManager.abandonAudioFocus(afChangeListener);
                     Log.d(NAME, "==> Audio Session Interruption case AUDIOFOCUS_LOSS.");
+                    _isCall = true;
                     stop();
                     break;
                   default:
